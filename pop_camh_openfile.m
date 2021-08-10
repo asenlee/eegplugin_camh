326301ce088174148d0d800aa110eead011e5451
Asen Lee <woochang.lee0405@gmail.com>
2021-08-10 10:55:12 -0700

    Update pop_camh_openfile.m

--- pop_camh_openfile.m
+++ pop_camh_openfile.m
@@ -10,10 +10,11 @@ end
 % pop up window
 
 if nargin < 2
-  geometry = {1 1}
+  geometry = { 1 [1 1] }
 
-  utilist = {{'style', 'text', 'string', 'OPEN FILE'} ...
-             {'style', 'text', 'string', 'return'}}
+  utilist = {{'style', 'text', 'string', 'Select File to Open'}
+             {'style', 'pushbutton', 'string', 'OPEN FILE', 'callback', 'Z_getSetsFileNames();'} ...
+             {'style', 'pushbutton', 'string', 'CANCEL', 'callback', 'close(gcbf);'}}
 
   result = inputgui('geometry', geometry, 'utilist', utilist, 'title', 'OPEN FILE -- pop_camh_openfile', 'helpcom', 'pophelp(''pop_camh_openfile'')');
   if isempty(result), return; end;
