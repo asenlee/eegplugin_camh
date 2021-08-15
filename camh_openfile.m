962e74cd00b4a772e6e86e5201e032339c115a3d
Asen Lee <woochang.lee0405@gmail.com>
2021-08-15 11:58:30 -0700

    edit

--- camh_openfile.m
+++ camh_openfile.m
@@ -2,4 +2,6 @@ function EEG = camh_openfile(EEG,varargin)
 
 if nargin < 1
     error('Not enough input arguments.');
+else
+    Z_getSetsFileNames();
 end
--- pop_camh_openfile.m
+++ pop_camh_openfile.m
@@ -14,7 +14,7 @@ if nargin < 2
   geometry = { 1 [1 1] }
 
   utilist = {{'style', 'text', 'string', 'Select File to Open'}
-             {'style', 'pushbutton', 'string', 'OPEN FILE', 'callback', 'Z_getSetsFileNames();'} ...
+             {'style', 'pushbutton', 'string', 'OPEN FILE', 'callback', 'camh_openfile;'} ...
              {'style', 'pushbutton', 'string', 'CANCEL', 'callback', 'close(gcbf);'}}
 
   result = inputgui('geometry', geometry, 'utilist', utilist, 'title', 'OPEN FILE -- pop_camh_openfile', 'helpcom', 'pophelp(''pop_camh_openfile'')');
